565c0621ee00ed306929c2cadd25b3e1765e5d73
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-11-20 22:34:22 +0700

    update button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function Button() {
+function Button({ text, href, className }) {
   return (
-    <div>Button</div>
+    <button type="button" className={`${className} py-2 px-7 bg-secondary-color text-white text-base font-semibold cursor-pointer max-w-fit rounded-full duration-200 hover:bg-[#079C89] sm:text-lg`}>
+      <Link to={href}>
+        {text}
+      </Link>
+    </button>
   );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  className: PropTypes.string,
 }
 
 export default Button;
