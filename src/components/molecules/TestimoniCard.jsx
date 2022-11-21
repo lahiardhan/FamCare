a49c0399c29a3c78308b0ab0cdf0af4d4b90cb8e
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-11-21 08:16:51 +0700

    menambah alt text image

--- src/components/molecules/TestimoniCard.jsx
+++ src/components/molecules/TestimoniCard.jsx
@@ -5,14 +5,14 @@ function TestimoniCard({ testi, index, testiIndex }) {
   const rating = [];
 
   for (let i = 0; i < testi.rating; i++) {
-    rating.push(<img key={i} src="/images/star.svg" />);
+    rating.push(<img key={i} src="/images/star.svg" alt="star-icon" />);
   };
 
   return (
     <div className={`min-h-[400px] w-full xs:min-h-[300px] md:min-h-[478px] lg:min-h-[364px] flex flex-col justify-between bg-[#ffffff] rounded-[30px] border border-secondary-color py-8 px-5 duration-300 ${index === testiIndex ? 'scale-100 opacity-100' : 'scale-75 lg:scale-[0.8] opacity-50'}`}>
       <div className="p-2 relative">
-        <img src="/images/up-quote.svg" className="w-5 absolute top-0 left-0" alt="" />
-        <img src="/images/down-quote.svg" className="w-5 absolute bottom-0 right-0" alt="" />
+        <img src="/images/up-quote.svg" className="w-5 absolute top-0 left-0" alt="up-quote icon" />
+        <img src="/images/down-quote.svg" className="w-5 absolute bottom-0 right-0" alt="down-quote icon" />
         <p>{testi.review}</p>
       </div>
       <div className="flex gap-3 mt-5">
--- src/components/organisms/HeroElement.jsx
+++ src/components/organisms/HeroElement.jsx
@@ -5,9 +5,9 @@ function HeroElement() {
   return (
     <div className="relative min-h-screen flex w-full bg-gradient-to-r from-primary-color to-secondary-color justify-center">
       <div className="w-full flex flex-col justify-center text-white max-w-[1200px] xs:grid xs:grid-cols-2 px-4 sm:px-8 lg:px-20 xl:px-32">
-        <img className="z-10 absolute bottom-0 left-0 w-screen" src="/images/wave.svg" alt="" />
-        <div className="flex justify-center h-fit xs:col-start-2 xs:row-start-1 xs:row-end-2 xs:items-start xs:relative xs:top-1/4"> {/** Penggunaan xs:relative membuat elemen ada di posisi z-index paling atas */}
-          <img src="/images/hero-image.svg" alt="" width={400} />
+        <img className="z-10 absolute bottom-0 left-0 w-screen" src="/images/wave.svg" alt="wave-image" />
+        <div className="flex justify-center min-h-[220px] xs:col-start-2 xs:row-start-1 xs:row-end-2 xs:items-start xs:relative xs:top-1/4"> {/** Penggunaan xs:relative membuat elemen ada di posisi z-index paling atas */}
+          <img src="/images/hero-image.svg" alt="hero-image" width={400} />
         </div>
         <div className="font-medium mt-6 h-fit xs:mt-0 xs:col-start-1 xs:row-start-1 xs:row-end-2 xs:relative xs:top-1/4 md:top-[30%]"> {/** Penggunaan xs:relative membuat elemen ada di posisi z-index paling atas */}
           <p className="text-sm sm:text-base lg:text-lg">Selamat datang di,</p>
