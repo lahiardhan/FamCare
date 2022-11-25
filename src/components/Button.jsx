799d083bf9205a0648458eed0fa4890360479c5c
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-11-25 19:13:33 +0700

    add new props: target

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function Button({ text, href, link, className }) {
+function Button({ text, href, link, target, className }) {
   return (
     <button type="button" className={`${className} py-2 px-7 bg-secondary-color text-white text-base font-semibold cursor-pointer max-w-fit rounded-full duration-200 hover:bg-[#079C89] sm:text-lg`}>
       {href ? 
-        <a href={href}>
+        <a href={href} target={target}>
           {text}
         </a>
         :
@@ -22,6 +22,8 @@ function Button({ text, href, link, className }) {
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   href: PropTypes.string,
+  link: PropTypes.string,
+  target: PropTypes.string,
   className: PropTypes.string,
 }
 
