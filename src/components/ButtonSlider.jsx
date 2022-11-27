24370b36cfd2ea14a15cb4f3f3a21f86a1bdaa87
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-11-27 16:34:34 +0700

    perbaikan penulisan attribute class= to className=

--- src/components/ButtonSlider.jsx
+++ src/components/ButtonSlider.jsx
@@ -6,12 +6,12 @@ function ButtonSlider({ value, onChange }) {
   return (
     <div className="flex flex-col">
       <input type="range" name="test" id="test" min="0" max="4" step="1" value={value} className="w-[300px] h-[15px] bg-secondary-color rounded-lg appearance-none cursor-pointer range-5xl dark:bg-gray-700" onChange={onChange}/>
-      <div class="flex justify-between mt-2 text-xs text-gray-600">
-        <span class="w-8 text-left">0</span>
-        <span class="w-8 text-center">1</span>
-        <span class="w-8 text-center">2</span>
-        <span class="w-8 text-center">3</span>
-        <span class="w-8 text-right">4</span>
+      <div className="flex justify-between mt-2 text-xs text-gray-600">
+        <span className="w-8 text-left">0</span>
+        <span className="w-8 text-center">1</span>
+        <span className="w-8 text-center">2</span>
+        <span className="w-8 text-center">3</span>
+        <span className="w-8 text-right">4</span>
       </div>
     </div>
   );
