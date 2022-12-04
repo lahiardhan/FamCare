33439ac3ebe14fbfe218fd5e9b26aaf548f434bd
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-12-04 22:30:32 +0700

    prettier code

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -6,9 +6,14 @@ import ProfilCard from '../../components/molecules/ProfilCard';
 import { Icon } from '@iconify/react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-AOS.init();
+import { useEffect } from 'react';
 
 function AboutUs() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   const NextArrow = ({ onClick }) => {
     return (
       <Icon
@@ -16,8 +21,8 @@ function AboutUs() {
         onClick={onClick}
         icon="material-symbols:arrow-forward-ios-rounded"
       />
-    )
-  }
+    );
+  };
 
   const PrevArrow = ({ onClick }) => {
     return (
@@ -26,8 +31,9 @@ function AboutUs() {
         onClick={onClick}
         icon="material-symbols:arrow-back-ios-rounded"
       />
-    )
-  }
+    );
+  };
+  
   const settings = {
     dots: true,
     infinite: true,
@@ -43,47 +49,33 @@ function AboutUs() {
       <div>
         <h1 className="font-extrabold text-4xl mt-20  text-center" data-aos="fade-down">About Us</h1>
       </div>
-      <div className='flex flex-wrap lg:px-32 '>
+      <div className='flex flex-wrap lg:px-32'>
         <div className='w-full self-center px-4 mt-7 md:w-1/2'>
-          <h1
-            className='text-2xl md:text-center font-bold text-secondary-color lg:text-4xl'
-            data-aos="fade-right"
-            data-aos-delay="200"
-            data-aos-easing="ease-in-sine"
-          >Tujuan Produk</h1>
-          <p
-            className='text-sm md:text-base mt-4 '
-            data-aos="fade-left"
-            data-aos-delay="300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veritatis ipsa, natus mollitia ut excepturi tenetur amet sed doloribus magnam laudantium, cupiditate maiores doloremque quibusdam sequi non laborum alias! Blanditiis corrupti inventore dolorum magnam eligendi maiores necessitatibus at officiis tempora</p>
+          <h1 className='text-2xl md:text-center font-bold text-secondary-color lg:text-4xl' data-aos="fade-right" data-aos-delay="200" data-aos-easing="ease-in-sine">Tujuan Produk</h1>
+          <p className='text-sm md:text-base mt-4' data-aos="fade-left" data-aos-delay="300">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veritatis ipsa, natus mollitia ut excepturi tenetur amet sed doloribus magnam laudantium, cupiditate maiores doloremque 
+            quibusdam sequi non laborum alias! Blanditiis corrupti inventore dolorum magnam eligendi maiores necessitatibus at officiis tempora
+          </p>
         </div>
         <div className='w-full self-end px-4 md:w-1/2'>
-          <div
-            className='mt-10'
-            data-aos="zoom-out"
-            data-aos-delay="500">
-            <img
-              src="/images/test-image.svg"
-              alt="hero-image"
-              width={400} />
+          <div className='mt-10' data-aos="zoom-out" data-aos-delay="500">
+            <img src="/images/test-image.svg" alt="hero-image" width={400} />
           </div>
         </div>
       </div>
       <div className='flex flex-wrap md:px-32 '>
         <div className='w-full self-end px-4 md:w-1/2'>
-          <div className='mt-10'
-            data-aos="zoom-out"
-            data-aos-delay="500">
+          <div className='mt-10' data-aos="zoom-out" data-aos-delay="500">
             <img src="/images/hero-image.svg" alt="hero-image" width={400} />
           </div>
         </div>
         <div className='w-full self-center px-4 mt-7 md:w-1/2'>
-          <h1
-            className='text-2xl font-bold text-secondary-color md:text-4xl md:text-center'
-            data-aos="fade-left"
-          >Misi Produk</h1>
-          <p className='text-sm md:text-base mt-4 ' data-aos="fade-right">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veritatis ipsa, natus mollitia ut excepturi tenetur amet sed doloribus magnam laudantium, cupiditate maiores doloremque quibusdam sequi non laborum alias! Blanditiis corrupti inventore dolorum magnam eligendi maiores necessitatibus at officiis tempora</p>
+          <h1 className='text-2xl font-bold text-secondary-color md:text-4xl md:text-center' data-aos="fade-left">Misi Produk</h1>
+          <p className='text-sm md:text-base mt-4' data-aos="fade-right">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veritatis ipsa, natus mollitia ut excepturi tenetur amet sed doloribus magnam laudantium, cupiditate maiores doloremque 
+            quibusdam sequi non laborum alias! Blanditiis corrupti inventore dolorum magnam eligendi maiores necessitatibus at officiis tempora
+          </p>
         </div>
-
       </div>
       <div>
         <h1 className="font-extrabold text-secondary-color text-4xl mt-24 lg:py-7 text-center" data-aos="fade-right">Meet Our Great Team</h1>
@@ -91,15 +83,11 @@ function AboutUs() {
       <Slider {...settings} className='mb-10 mx-2 sm:mx-16 md:mx-32 lg:mx-[400px]'>
         {profilList.map((profil) => (
           <ProfilCard profil={profil} />
-
-
         ))}
       </Slider>
-
       <Footer />
     </section>
-
   );
-}
+};
 
 export default AboutUs;
