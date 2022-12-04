7226e473c6dde18a0e1f5b81888385143e6c4708
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-12-04 22:22:29 +0700

    add padding and margin

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,7 +10,7 @@ function Page404() {
   } ,[]);
 
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center">
+    <section className="min-h-screen flex flex-col justify-center items-center pt-20">
       <div className="w-full max-w-[600px] flex flex-col items-center">
         <img src="/images/page-not-found.svg" alt="404-[Page Not Found]" className="w-full" data-aos="zoom-in" />
         <div data-aos="zoom-in">
