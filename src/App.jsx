627f22863e3c9f1c48ac3ffb712589d6627ee68a
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-12-04 19:51:45 +0700

    move Navbar to folder organismss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Loadable from 'react-loadable';
-import Navbar from './components/Navbar';
+import Navbar from './components/organisms/Navbar';
 import Loading from './components/Loading';
 import { getArticleList } from './services/news-api';
 
