c164656f5067de2acec6505a9482165617e6316c
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-12-05 15:28:04 +0700

    merapihkan tampilan styling page article

--- src/pages/ArticlePage/index.jsx
+++ src/pages/ArticlePage/index.jsx
@@ -18,9 +18,9 @@ function Article({ articles }) {
 
   if (loading) {
     return (
-      <section>
+      <section className="min-h-screen pt-20">
         <div>
-          <h1 className="font-extrabold text-4xl mt-24 lg:py-7 text-center">Artikel FamCare</h1>
+          <h1 className="font-extrabold text-4xl mt-4 lg:py-7 text-center">Artikel FamCare</h1>
         </div>
         <div className="lg:px-36">
           <Skeleton />
@@ -33,26 +33,30 @@ function Article({ articles }) {
 
   return (
     <>
-      <section>
+      <section className="min-h-screen pt-20">
         <div>
-          <h1 className="font-extrabold text-4xl mt-24 lg:py-7 text-center">Artikel FamCare</h1>
+          <h1 className="font-extrabold text-4xl mt-4 lg:py-7 text-center">Artikel FamCare</h1>
         </div>
-        <div className="lg:px-36">
-          {articles.map((article, index) => (
-            <div key={index} className="px-8 py-4 mb-5 w-full lg:flex lg:mb-8 justify-center lg:gap-10">
-              <div className="w-full md:items-center mb-5">
-                <img src={article.urlToImage ? article.urlToImage : '/images/thumbnailArticleDefault.webp'} alt={`Image ${article.title}`} className="object-cover max-h-72 w-full rounded-tl-[50px] rounded-br-[50px]" />
-              </div>
+        {articles.length !== 0 ? (
+          <div className="lg:px-36">
+            {articles.map((article, index) => (
+              <div key={index} className="px-8 py-4 mb-5 w-full lg:flex lg:mb-8 justify-center lg:gap-10">
+                <div className="w-full md:items-center mb-5">
+                  <img src={article.urlToImage ? article.urlToImage : '/images/thumbnailArticleDefault.webp'} alt={`Image ${article.title}`} className="object-cover max-h-72 w-full rounded-tl-[50px] rounded-br-[50px]" />
+                </div>
 
-              <div className="text-black lg:max-w-[475px]">
-                <h3 className="text-xl md:text-2xl lg:text-3xl font-bold duration-300 hover:opacity-70"><a href={article.url} target="_blank">{article.title}</a></h3>
-                <p className="text-xs lg:text-sm font-light lg:mb-3">{showFormattedDate(article.publishedAt)} | {article.author ? article.author : 'N/A'}</p>
-                <p className="text-sm lg:text-base mb-4 lg:mb-5 line-clamp-2">{article.description}</p>
-                <Button href={article.url} target="_blank" text="Baca Selengkapnya" className="mt-2" />
+                <div className="text-black lg:max-w-[475px]">
+                  <h3 className="text-xl md:text-2xl lg:text-3xl font-bold duration-300 hover:opacity-70"><a href={article.url} target="_blank">{article.title}</a></h3>
+                  <p className="text-xs lg:text-sm font-light lg:mb-3">{showFormattedDate(article.publishedAt)} | {article.author ? article.author : 'N/A'}</p>
+                  <p className="text-sm lg:text-base mb-4 lg:mb-5 line-clamp-2">{article.description}</p>
+                  <Button href={article.url} target="_blank" text="Baca Selengkapnya" className="mt-2" />
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center h-[83vh]">something went wrong</div>
+        )}
       </section>
       <Footer />
     </>
