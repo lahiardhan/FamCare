ff04d1e0cf82e0b226e14077bff0221bba4af758
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2022-12-08 22:30:27 +0700

    fix search fiture at forum discussion

--- src/services/discussion.services.js
+++ src/services/discussion.services.js
@@ -1,10 +1,22 @@
-import { db } from "../utils/firebase-config";
+import {
+    db
+} from "../utils/firebase-config";
 
-import { collection, getDoc, addDoc, updateDoc, deleteDoc, doc, getDocs } from "firebase/firestore";
+import {
+    collection,
+    getDoc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+    getDocs,
+    query,
+    where
+} from "firebase/firestore";
 
 const discussionCollectionRef = collection(db, "discussions");
 
-class DiscussionDataService{
+class DiscussionDataService {
     addDiscussions = (newDiscussion) => {
         return addDoc(discussionCollectionRef, newDiscussion);
     }
@@ -26,6 +38,12 @@ class DiscussionDataService{
     getDiscussion = (id) => {
         const discussionDoc = doc(db, "discussions", id);
         return getDoc(discussionDoc)
+    }
+    searchByTitle = async (params) => {
+        const q = query(discussionCollectionRef, where("title", "==", params));
+        const res = await getDocs(q);
+        return res;
+        // res.forEach((doc) => doc.data())
     }
 }
 
