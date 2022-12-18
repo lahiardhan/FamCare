ad662ebfbaac0ae29c7e0ab6e78747f0ddad90e8
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2022-12-18 20:48:42 +0700

    fix forum diskusi

--- src/components/Discussion/CommentList.jsx
+++ src/components/Discussion/CommentList.jsx
@@ -4,9 +4,25 @@ import { useState } from "react";
 import { FaRegCommentDots } from "react-icons/fa";
 import CommentDataService from "../../services/comments.services";
 import showFormattedDate from "../../utils/formattedDate";
+import Cookies from 'js-cookie'
+import jwtDecode from 'jwt-decode';
 
 function CommentList() {
   const [comments, setComments] = useState([]);
+  const [user, setUser] = useState({
+    username: 'user',
+  });
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = atob(token);
+      const payLoad = jwtDecode(jwtToken)
+      const userFromPlayLoad = payLoad.player;
+      user.username = `${userFromPlayLoad.username}`;
+      setUser(user);
+    }
+  }, []);
 
   useEffect(() => {
     getComments();
@@ -25,7 +41,6 @@ function CommentList() {
         return (
           <div className="mt-2 border border-slate-200 rounded-md shadow-md">
             <div className="w-10/12 p-10">
-              <h1 className="text-md font-medium">{doc.date}</h1>
               <p key={index}>{doc.comments}</p>
               <div className="flex items-center mt-3 space-x-2"></div>
             </div>
